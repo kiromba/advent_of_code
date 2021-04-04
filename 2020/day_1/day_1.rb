b2f7cbf38a6ecbc6c627c637cf97da0741e32795
Diogo Viana <diogo.abreuviana@gmail.com>
2021-04-05 01:14:23 +0200

    finishing day 1

--- 2020/day_1/day_1.rb
+++ 2020/day_1/day_1.rb
@@ -11,3 +11,16 @@ for i in numbers do
 end
 
 puts found.inject(:*)
+
+# part 2
+for i in numbers do
+  for j in numbers do
+    for k in numbers do
+      next if i == j && j == k
+      found = [i, j, k] if i + j + k == 2020
+    end
+  end
+end
+
+puts found.inspect
+puts found.inject(:*)
